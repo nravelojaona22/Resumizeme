aef3c1d6f9ce13323f0f5c071489fd36c02ef50e
Nah <nah642637@gmail.com>
2024-02-06 07:04:27 +0300

    Updates

--- static/ycbg2HS_N9QtUVdKYAMnC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/15-8753e542df85c135.js","static/chunks/pages/index-d518dbce1cdd07ce.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
